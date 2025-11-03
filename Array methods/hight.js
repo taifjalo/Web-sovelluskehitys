f451a2f75e2e22696cce5d49eb7fb4f91362796f
taif <taifjalo0@gmail.com>
2025-11-03 21:43:57 +0200

    Advanced JavaScript For Restaurant App.

--- Array methods/hight.js
+++
@@ -1,9 +0,0 @@
-function operationOnNumbers(a, b, operation) {
-  return operation(a, b);
-}
-
-const add = (x, y) => x + y;
-const subtract = (x, y) => x - y;
-
-console.log(operationOnNumbers(5, 3, add)); // 8
-console.log(operationOnNumbers(10, 4, subtract)); // 6
