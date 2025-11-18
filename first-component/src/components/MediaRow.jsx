97aa175e1e2dc89c639108105fd2f94b8a36b5ed
taif <taifjalo0@gmail.com>
2025-11-18 22:31:02 +0200

    First components exercise

---
+++ first-component/src/components/MediaRow.jsx
@@ -0,0 +1,32 @@
+// src/components/MediaRow.jsx
+import PropTypes from 'prop-types';
+
+const MediaRow = (props) => {
+  const {item} = props;
+  return (
+    <tr key={item.media_id}>
+      <td>
+        <img src={item.thumbnail} alt={item.title} />
+      </td>
+      <td>{item.title}</td>
+      <td>{item.description}</td>
+      <td>{new Date(item.created_at).toLocaleString('fi-FI')}</td>
+      <td>{item.filesize}</td>
+      <td>{item.media_type}</td>
+    </tr>
+  );
+};
+
+MediaRow.propTypes = {
+  item: PropTypes.shape({
+    media_id: PropTypes.number.isRequired,
+    thumbnail: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string,
+    created_at: PropTypes.string.isRequired,
+    filesize: PropTypes.number.isRequired,
+    media_type: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default MediaRow;
