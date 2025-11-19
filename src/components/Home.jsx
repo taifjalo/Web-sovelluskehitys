47331a28c6ec2ba46d05626aa5e1392f5ffb7724
taif <taifjalo0@gmail.com>
2025-11-19 14:04:15 +0200

    State management for SingleView selection.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,46 +1,45 @@
 import MediaRow from './MediaRow';
 import SingleView from './SingleView';
-import { useState } from 'react';
-
+import {useState} from 'react';
 
 const Home = () => {
   const [selectedItem, setSelectedItem] = useState(null);
 
   const mediaArray = [
-  {
-    media_id: 8,
-    user_id: 5,
-    filename: '...',
-    thumbnail: '...',
-    filesize: 170469,
-    media_type: 'image/jpeg',
-    title: 'Picture 1',
-    description: 'This is a placeholder picture.',
-    created_at: '2024-01-07T20:49:34.000Z',
-  },
-  {
-    media_id: 9,
-    user_id: 7,
-    filename: '...',
-    thumbnail: '...',
-    filesize: 1002912,
-    media_type: 'image/jpeg',
-    title: 'Pic 2',
-    description: '',
-    created_at: '2024-01-07T21:32:27.000Z',
-  },
-  {
-    media_id: 17,
-    user_id: 2,
-    filename: '...',
-    thumbnail: '...',
-    filesize: 1236616,
-    media_type: 'video/mp4',
-    title: 'Bunny',
-    description: 'Butterflies fly around the bunny.',
-    created_at: '2024-01-07T20:48:13.000Z',
-  },
-];
+    {
+      media_id: 8,
+      user_id: 5,
+      filename: '...',
+      thumbnail: '...',
+      filesize: 170469,
+      media_type: 'image/jpeg',
+      title: 'Picture 1',
+      description: 'This is a placeholder picture.',
+      created_at: '2024-01-07T20:49:34.000Z',
+    },
+    {
+      media_id: 9,
+      user_id: 7,
+      filename: '...',
+      thumbnail: '...',
+      filesize: 1002912,
+      media_type: 'image/jpeg',
+      title: 'Pic 2',
+      description: '',
+      created_at: '2024-01-07T21:32:27.000Z',
+    },
+    {
+      media_id: 17,
+      user_id: 2,
+      filename: '...',
+      thumbnail: '...',
+      filesize: 1236616,
+      media_type: 'video/mp4',
+      title: 'Bunny',
+      description: 'Butterflies fly around the bunny.',
+      created_at: '2024-01-07T20:48:13.000Z',
+    },
+  ];
   return (
     <>
       <h2>My Media</h2>
@@ -60,9 +59,10 @@ const Home = () => {
         <tbody>
           {mediaArray.map((item) => (
             <MediaRow
-            key={item.media_id}
-            item={item}
-            setSelectedItem={setSelectedItem} />
+              key={item.media_id}
+              item={item}
+              setSelectedItem={setSelectedItem}
+            />
           ))}
         </tbody>
       </table>
