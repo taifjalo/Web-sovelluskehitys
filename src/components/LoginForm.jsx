6df7ea0727f98e8260e62a8bd08e963122b114de
taif <taifjalo0@gmail.com>
2025-11-22 18:11:52 +0200

    .

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,9 +1,31 @@
 const LoginForm = () => {
   return (
-    <div>
-      <h2>LoginForm Page</h2>
-      <p>You have been LoginForm.</p>
-    </div>
+    <>
+      <h1>Login</h1>
+      <form onSubmit={() => {}}>
+        <div>
+          <label htmlFor="loginuser">Username</label>
+          <input
+            name="username"
+            type="text"
+            id="loginuser"
+            onChange={() => {}}
+            autoComplete="username"
+          />
+        </div>
+        <div>
+          <label htmlFor="loginpassword">Password</label>
+          <input
+            name="password"
+            type="password"
+            id="loginpassword"
+            onChange={() => {}}
+            autoComplete="current-password"
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </>
   );
 };
 
